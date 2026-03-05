21025a2c6867f1b6f598e4660b8996ae0a913c76
maxcollo82 <magicdarkness@gmail.com>
2026-03-05 20:07:43 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Inizio installazione software per Asti..."
+sudo apt update
+sudo apt install -y chromium-browser vlc gparted onlyoffice-desktopeditors
+sudo apt autoremove -y
+echo "Installazione completata!"
