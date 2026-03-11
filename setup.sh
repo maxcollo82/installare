a07e730c28a2b46bdfc259264dc632844aea66b6
maxcollo82 <magicdarkness@gmail.com>
2026-03-11 14:44:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Inizio installazione software per Asti..."
+echo "Inizio installazione software..."
 sudo apt update
 sudo apt install -y chromium-browser vlc gparted onlyoffice-desktopeditors
 sudo apt autoremove -y
