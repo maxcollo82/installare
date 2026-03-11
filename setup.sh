0dbfa69d26845605a4f2bac6577b79e8b0f68789
maxcollo82 <magicdarkness@gmail.com>
2026-03-11 14:44:38 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Inizio installazione software..."
-sudo apt update
-sudo apt install -y chromium-browser vlc gparted onlyoffice-desktopeditors
-sudo apt autoremove -y
-echo "Installazione completata!"
